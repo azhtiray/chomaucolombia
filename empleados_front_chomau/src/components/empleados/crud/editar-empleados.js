6345fec335661cd49b7f7635a2c5cd998674611e
Yarithza <azhtiray@gmail.com>
2022-11-23 19:02:59 -0500

    Ventana emergente de editar empleado Se ejecuta ventana emergente de si desea confirmar editar el empleado

--- empleados_front_chomau/src/components/empleados/crud/editar-empleados.js
+++ empleados_front_chomau/src/components/empleados/crud/editar-empleados.js
@@ -4,6 +4,7 @@ import '../empleados.css';
 import { request } from '../../helper/helper';
 import Loading from '../../loading/loading'
 import MessagePrompt from '../../prompt/mesage';
+import ConfirmationPrompt from '../../prompt/confirmation';
 
 export default class EmpleadosEditar extends React.Component {
     constructor(props) {
@@ -16,8 +17,8 @@ export default class EmpleadosEditar extends React.Component {
                 show: false,
             },
             confirmation: {
-                title: "",
-                text: "",
+                title: "Modificar Empleado",
+                text: "¿Desea modificar el empleado?",
                 show: false,
             },
             loading: false,
@@ -30,6 +31,8 @@ export default class EmpleadosEditar extends React.Component {
             },
         };
         this.onExitedMessage = this.onExitedMessage.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+        this.onConfirm = this.onConfirm.bind(this);
     }
     componentDidMount() {
         this.getEmpleado();
@@ -66,7 +69,6 @@ export default class EmpleadosEditar extends React.Component {
                     this.props.changeTab('buscar');
                 }
                 this.setState({ loading: false });
-
             })
             .catch((err) => {
                 console.error(err);
@@ -76,6 +78,26 @@ export default class EmpleadosEditar extends React.Component {
     onExitedMessage() {
         if (this.state.rediret) this.props.changeTab('buscar');         // es rediret 
     }
+    onCancel() {
+        this.setState({
+            confirmation: {
+                ...this.state.confirmation,
+                show: false,
+            },
+        })
+    }
+    onConfirm() {
+        this.setState({
+            confirmation: {
+                ...this.state.confirmation,
+                show: false,
+            },
+        },
+       this.guardarEmpleados()
+        );
+    }
+
+
     render() {
         return (
             <Container id='empleados-editar-container'>
@@ -85,7 +107,13 @@ export default class EmpleadosEditar extends React.Component {
                     duration={2500}
                     onExited={this.onExitedMessage}
                 />
-
+                <ConfirmationPrompt
+                    show={this.state.confirmation.show}
+                    title={this.state.confirmation.title}
+                    text={this.state.confirmation.text}
+                    onCancel={this.onCancel}
+                    onConfirm={this.onConfirm}
+                />
                 <Loading show={this.state.loading} />
                 <Row>
                     <h1> Editar Empleado</h1>
@@ -97,51 +125,47 @@ export default class EmpleadosEditar extends React.Component {
                             <Form.Control
                                 value={this.state.empleado.nombres}
                                 onChange={(e) => this.setValue("nombres", e.target.value)} />
-
                         </Form.Group>
-
                         <Form.Group className="mb-3" controlId="formbasic">
                             <Form.Label>Apellidos</Form.Label>
                             <Form.Control
                                 value={this.state.empleado.apellidos}
                                 onChange={(e) => this.setValue("apellidos", e.target.value)} />
                         </Form.Group>
-
                         <Form.Group className="mb-3" controlId="formbasic">
                             <Form.Label>Direccion</Form.Label>
                             <Form.Control
                                 value={this.state.empleado.direccion}
                                 onChange={(e) => this.setValue("direccion", e.target.value)} />
                         </Form.Group>
-
                         <Form.Group className="mb-3" controlId="formbasic">
                             <Form.Label>Telefono</Form.Label>
                             <Form.Control
                                 value={this.state.empleado.telefono}
                                 onChange={(e) => this.setValue("telefono", e.target.value)} />
                         </Form.Group>
-
-
                         <Form.Group className="mb-3" controlId="formbasic">
                             <Form.Label>Correo</Form.Label>
                             <Form.Control
                                 value={this.state.empleado.mail}
                                 onChange={(e) => this.setValue("mail", e.target.value)} />
                         </Form.Group>
-
-                        {/* <Button variant="primary" 
-                        onClick={() => this.setState({
-                            confirmation: {...this.state.confirmation, show:true,}
-                        })}> */}
-
-                        <Button onClick={() => console.log(this.guardarEmpleados())}>
-                            Guardar Editar Empleado
+                        <Button variant="primary"
+                            onClick={() => this.setState({
+                                confirmation: {
+                                    ...this.state.confirmation, show: true,
+                                }
+                            })}>
+                            Guardar Editar Empleados
                         </Button>
                     </Form>
                 </Row>
-
             </Container>
         );
     }
 }
 
+
+//  {/* { <Button onClick={() => console.log(this.guardarEmpleados())}>
+//                                 Guardar Editar Empleados
+//                             } */}
--- empleados_front_chomau/src/components/prompt/mesage.js
+++ empleados_front_chomau/src/components/prompt/mesage.js
@@ -8,27 +8,26 @@ export default class MessagePrompt extends React.Component {
         super(props);
         this.state = {
             show: false,
-        }
+        };
     }
-
     componentWillReceiveProps(nextProps) {
         if (nextProps.show) this.setState({ show: true }, this.hideMessage());
     }
-    hideMessage(){
+    hideMessage() {
         setTimeout(() => {
-            this.setState({ show: false});
+            this.setState({ show: false });
         }, this.props.duration);
     }
-    onExited(){
-        if(!isUndefined(this.props.onExited) && !isNull(this.props.onExited))
-        this.props.onExited();
+    onExited() {
+        if (!isUndefined(this.props.onExited) && !isNull(this.props.onExited))
+            this.props.onExited();
     }
     render() {
         return (
             <Container>
                 <Modal id=" message-prompt" centered show={this.state.show}
                     onExited={() => this.onExited()}
-                    >
+                >
                     <ModalBody>
                         {this.props.text}
                     </ModalBody>
